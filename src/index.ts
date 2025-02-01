8d9fe7f0c68e14d0282d86b2a47939478afe955e
Abees FM <abeesdevjs@gmail.com>
2025-02-02 00:51:16 +0700

    init source

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import {
+  InteractionResponseType,
+  InteractionType,
+  verifyKeyMiddleware,
+} from 'discord-interactions';
+
+console.log(process.env.CLIENT_PUBLIC_KEY);
+
+const bootstrap = async () => {
+  const app = express();
+  const port = 3000;
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(cors());
+  app.use(express.json());
+
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Hello World!' });
+  });
+
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+
+  app.post(
+    '/interactions',
+    verifyKeyMiddleware(process.env.CLIENT_PUBLIC_KEY),
+    (req, res) => {
+      const { type, id, data } = req.body;
+
+      /**
+       * Handle verification requests
+       */
+      if (type === InteractionType.PING) {
+        res.send({ type: InteractionResponseType.PONG });
+      }
+    },
+  );
+};
+
+void bootstrap();
