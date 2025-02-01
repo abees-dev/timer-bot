15a53e4234a824929ffa2ec4d2707140b7d90fe7
Abees FM <abeesdevjs@gmail.com>
2025-02-02 02:06:16 +0700

    refactor middleware to conditionally apply JSON parsing for interactions endpoint

--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,18 @@ import {
 } from 'discord-interactions';
 import { createTimerCommand } from './commands/timer';
 
-console.log(process.env.CLIENT_PUBLIC_KEY);
-
 const bootstrap = async () => {
   const app = express();
   const port = 3000;
-
-  app.use(express.urlencoded({ extended: true }));
   app.use(cors());
-  app.use(express.json());
+
+  app.use((req, res, next) => {
+    if (req.path === '/interactions') {
+      next(); // Skip middleware for interactions
+    } else {
+      express.json()(req, res, next);
+    }
+  });
   app.set('view engine', 'ejs');
   app.get('/', (req, res) => {
     res.status(200).json({ message: 'Hello World!' });
@@ -30,23 +33,19 @@ const bootstrap = async () => {
     res.render('privacy', { title: 'Privacy Policy' });
   });
 
-  app.post(
-    '/interactions',
-    verifyKeyMiddleware(process.env.CLIENT_PUBLIC_KEY),
-    (req, res) => {
-      const { type, id, data } = req.body;
-      console.log('Received interaction', req.body);
+  app.post('/interactions', (req, res) => {
+    const { type, id, data } = req.body;
+    console.log('Received interaction', req.body);
 
-      /**
-       * Handle verification requests
-       */
-      if (type === InteractionType.PING) {
-        res.send({ type: InteractionResponseType.PONG });
-      }
-
+    /**
+     * Handle verification requests
+     */
+    if (type === InteractionType.PING) {
       res.send({ type: InteractionResponseType.PONG });
-    },
-  );
+    }
+
+    res.send({ type: InteractionResponseType.PONG });
+  });
 
   app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
